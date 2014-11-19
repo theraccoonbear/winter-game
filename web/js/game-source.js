5cdddd8858a5a6bb59c6a8ed5c894ebb3ab651f8
dsmith <dsmith@shineunited.com>
2014-11-19 15:34:16 -0600

    zoom dialog

--- web/js/game-source.js
+++ web/js/game-source.js
@@ -909,18 +909,19 @@ var GAME = BASE.extend({
 				src: opts.id,
 				type: 'inline'
 			},
+			mainClass: 'mfp-zoom-in',
 			//mainClass: 'mfp-with-anim', // this class is for CSS animation below
 			zoom: {
 				enabled: true, // By default it's false, so don't forget to enable it
-		
+			
 				duration: 300, // duration of the effect, in milliseconds
 				easing: 'ease-in-out', // CSS transition easing function 
-		
+			
 				// The "opener" function should return the element from which popup will be zoomed in
 				// and to which popup will be scaled down
 				// By defailt it looks for an image tag:
 				opener: function(openerElement) {
-					return $('canvas');
+					return $('body');
 				//	// openerElement is the element on which popup was initialized, in this case its <a> tag
 				//	// you don't need to add "opener" option if this code matches your needs, it's defailt one.
 				//	return openerElement.is('img') ? openerElement : openerElement.find('img');
